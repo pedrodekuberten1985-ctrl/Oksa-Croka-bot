664fc40fd79e23968a01f21b1c7f8209868d726b
pedrodekuberten1985-ctrl <pedrodekuberten1985@gmail.com>
2025-12-01 04:51:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,12 +6,10 @@ async def start(update, context):
     await update.message.reply_text("Привет! Я OksaCroka_bot ❤️")
 
 async def help_command(update, context):
-    await update.message.reply_text("Я создан для тебя!")
+    await update.message.reply_text("Я всегда рядом для тебя!")
 
 def main():
     token = os.getenv("BOT_TOKEN")
-    if not token:
-        raise RuntimeError("❌ BOT_TOKEN не задан")
     app = Application.builder().token(token).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
