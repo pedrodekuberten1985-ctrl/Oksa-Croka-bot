7892fffcf0caeb9daa7b49b8838a87243f351f5e
pedrodekuberten1985-ctrl <pedrodekuberten1985@gmail.com>
2025-12-01 04:36:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,14 +9,9 @@ async def help_command(update, context):
 
 def main():
     token = os.getenv("BOT_TOKEN")
-    if not token:
-        raise RuntimeError("❌ BOT_TOKEN не найден! Убедись, что он добавлен в переменные окружения.")
-
     app = Application.builder().token(token).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
-
-    print("✅ Бот запущен и работает на Render!")
     app.run_polling()
 
 if __name__ == "__main__":
