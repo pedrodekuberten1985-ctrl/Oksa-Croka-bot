b50c8ea304d4dda03a68e342e288adc2fe65382c
pedrodekuberten1985-ctrl <pedrodekuberten1985@gmail.com>
2025-12-01 03:38:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,11 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 import os
 
+# Получаем токен из переменных окружения
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Я OksaCroka_bot ??")
+    await update.message.reply_text("Привет! Я OksaCroka_bot ❤️")
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Я всегда рядом для тебя!")
@@ -14,7 +15,7 @@ def main():
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
-    print("? Бот запущен и работает на Railway!")
+    print("✅ Бот запущен и работает на Render!")
     app.run_polling()
 
 if __name__ == '__main__':
